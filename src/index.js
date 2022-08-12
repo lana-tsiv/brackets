a41b62073e7ad22d9c6f586eb3f7e60be2f83ae1
Lana <tiushkievich00@mail.ru>
2022-08-12 19:52:22 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const charArr = [...str];
+
+  const checkEnd = (i) => {
+    let result = true;
+    bracketsConfig.forEach((b) => {
+      if (b[1] === i) result = b[0];
+    });
+    return result;
+  };
+
+  for (let i = 0; i < charArr.length; i++) {
+    if (checkEnd(charArr[i]) !== false) {
+      if (charArr[i - 1] === checkEnd(charArr[i])) {
+        charArr.splice(i - 1, 2);
+        i = 0;
+      }
+    }
+  }
+
+  return charArr.length > 0 ? false : true;
+};
